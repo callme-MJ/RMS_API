57700bdbb3484e6fb598e0bebfe9f8d14d7ae10b
sinankmuriyanal <sinankmuriyanal@gmail.com>
2022-10-12 17:47:53 +0530

    review mistakes corrected

--- src/institute/controllers/institute.controller.ts
+++ src/institute/controllers/institute.controller.ts
@@ -1,25 +1,22 @@
 import {
+  Body,
   Controller,
-  Req,
-  HttpStatus,
   Delete,
+  Get,
   Param,
   ParseIntPipe,
-  Get,
-  Body,
   Post,
+  Req,
   Res,
+  UploadedFile,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
-  UseInterceptors,
-  UploadedFile,
-  Query,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
 import { storage } from '../../utils/storage.config';
 import { CandidateDTO } from '../dtos/candidate.dto';
-import { Candidate } from '../entities/candidate.entity';
 import { instituteService } from '../services/institute.service';
 
 @Controller('candidates')
@@ -27,18 +24,37 @@ export class instituteController {
   constructor(private readonly instituteService: instituteService) {}
 
   @Get('/')
-  async getAllCandidates():Promise<Candidate[]> {
-    return await this.instituteService.findAllCandidates();
+  async getCandidate(@Req() req: Request) {
+    const candidates = await this.instituteService.queryBuilder('candidates');
+
+    if (req.query.search) {
+      candidates.where(
+        'candidates.name LIKE :search OR candidates.ad_no LIKE :search OR candidates.chest_No LIKE :search',
+        { search: `%${req.query.search}%` },
+      );
+    }
+
+    const sort: any = req.query.sort;
+    if (sort) {
+      candidates.orderBy('candidates.name', sort.toUpperCase()).getMany();
+    }
+
+    const page: number = parseInt(req.query.page as any) || 1;
+    const perPage = 15;
+    candidates.offset((page - 1) * perPage).limit(perPage);
+    const [data, total] = await candidates.getManyAndCount();
+
+    return { data, total };
   }
 
   @Post('/')
   @UseInterceptors(FileInterceptor('file', { storage }))
   @UsePipes(ValidationPipe)
   async createCandidate(
-    @Body() CandidateDTO: CandidateDTO,
+    @Body() candidateDTO: CandidateDTO,
     @UploadedFile() file,
   ) {
-    return await this.instituteService.createCandidate(CandidateDTO, file);
+    return await this.instituteService.createCandidate(candidateDTO, file);
   }
 
   @Get('/:chest_No')
@@ -46,43 +62,11 @@ export class instituteController {
     @Res() response,
     @Param('chest_No', ParseIntPipe) chest_NO: number,
   ) {
-    const candidate = await this.instituteService.findCandidateByChestNo(chest_NO);
-    
-    return response.status(HttpStatus.OK).json({
-      candidate,
-    });
+    return await this.instituteService.findCandidateByChestNo(chest_NO);
   }
 
   @Delete('/:id')
   async deleteUserById(@Param('id', ParseIntPipe) id: number) {
     return this.instituteService.deleteCandidate(id);
-  }
-
-  @Get('/')
-  async querybuilder(@Req() req: Request) {
-    const builder = await this.instituteService.queryBuilder('candidates');
-
-    if (req.query.search) {
-      builder.where(
-        'candidates.id LIKE :search OR candidates.name LIKE :search OR candidates.ad_no LIKE :search',
-        { search: `%${req.query.search}%` },
-      );
-    }
-
-    const sort: any = req.query.sort;
-    if (sort) {
-      builder.orderBy('candidates.id', sort.toUpperCase());
-    }
-
-    const page: number = parseInt(req.query.page as any) || 1;
-    const perPage = 3;
-    builder.offset((page - 1) * perPage).limit(perPage);
-    const total = await builder.getCount();
-
-    return {
-      data: await builder.getMany(),
-      total,
-      page,
-    };
   }
 }

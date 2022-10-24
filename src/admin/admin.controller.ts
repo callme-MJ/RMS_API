d16e30942ecaaef4e63ab43affadf179d6c0835c
sinankmuriyanal <sinankmuriyanal@gmail.com>
2022-10-24 07:18:39 +0530

    env details added

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,7 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+
+
 }

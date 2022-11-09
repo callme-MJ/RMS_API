20ca82824ea2bb9f625699a6040c91776fadd72b
sinankmuriyanal <sinankmuriyanal@gmail.com>
2022-11-10 00:12:29 +0530

    connected elimination result and candidate program

--- src/elimination-result/entities/elimination-result.entity.ts
+++ src/elimination-result/entities/elimination-result.entity.ts
@@ -42,7 +42,7 @@ export class EliminationResult {
   @Column({ nullable: true })
   remarks: string;
 
-  @OneToOne( type => CandidateProgram)
+  @OneToOne( type => CandidateProgram ,{eager:true})
   @JoinColumn()
   candidateProgram: CandidateProgram;
 }

2fa9258fd017b56655ecd1fd792bc07595305bf8
dulkifal <dulkifilibacker@gmail.com>
2022-11-09 14:38:33 +0530

    ad

--- src/session/session.module.ts
+++ src/session/session.module.ts
@@ -1,13 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AdminSessionController } from './admin-session.controller';
+import { AdminSessionController } from './controllers/admin-session.controller';
+import { UserSessionController } from './controllers/user-session.controller';
 import { Session } from './entities/session.entity';
 import { SessionService } from './session.service';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([Session])],
-  controllers: [AdminSessionController],
+  controllers: [AdminSessionController,UserSessionController],
   providers: [SessionService],
   exports: [SessionService],
 })

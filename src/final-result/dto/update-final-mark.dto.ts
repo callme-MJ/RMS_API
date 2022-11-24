507b537df08ec8ff2cc7c6cead0541cc525e9507
sinankmuriyanal <sinankmuriyanal@gmail.com>
2022-11-24 06:48:47 +0530

    final-result

--- src/final-result/dto/update-final-mark.dto.ts
+++ src/final-result/dto/update-final-mark.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateFinalMarkDto } from './create-final-Mark.dto';
+import { CreateFinalMarkDto } from './create-final-mark.dto';
 
 export class UpdateFinalMarkDto extends PartialType(CreateFinalMarkDto) {}

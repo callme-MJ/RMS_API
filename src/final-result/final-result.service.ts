b5bfceec3ea62dcff95e27e5453ee80878d3e315
sinankmuriyanal <sinankmuriyanal@gmail.com>
2022-12-04 11:24:44 +0530

    edit

--- src/final-result/final-result.service.ts
+++ src/final-result/final-result.service.ts
@@ -583,7 +583,7 @@ export class FinalResultService {
         'candidate.category_id, candidateProgram.chestNO ,institute.short_name ,candidate.name as candidateName,candidate.photo ,category.name as categoryName,session.name as sessionName , session.id ',
       )
       .addSelect('SUM(candidateProgram.point)', 'score')
-      .groupBy('candidate.category_id, candidateProgram.chestNO ,institute.short_name ,candidate.name as candidateName,candidate.photo ,category.name as categoryName,session.name as sessionName , session.id ')
+      .groupBy('candidate.category_id, candidateProgram.chestNO ,institute.short_name ,candidate.name,candidate.photo ,category.name,session.name , session.id ')
       .orderBy('score', 'DESC')
       .getRawMany();
 
